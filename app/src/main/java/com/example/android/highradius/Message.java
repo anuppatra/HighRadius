a4dd5dc0423405854dd27f74cae06a7c44fc3d6c
Anup Kumar Patra <anup.patra97@gmail.com>
2018-07-02 19:43:10 +0530

    //done

---
+++ app/src/main/java/com/example/android/highradius/Message.java
@@ -0,0 +1,10 @@
+package com.example.android.highradius;
+
+
+
+public class Message{
+    public String idSender;
+    public String idReceiver;
+    public String text;
+    public long timestamp;
+}
